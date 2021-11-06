7c0103bf12b535d05424bbdc5f4c1190877a8f56
Aladiah96 <63356947+Aladiah96@users.noreply.github.com>
2021-11-06 14:32:30 -0300

    Initial commit

---
+++ function.js
@@ -0,0 +1,7 @@
+function toggle_visibility(resposta) {
+    var e = document.getElementsByClassName("resposta");
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+ }
