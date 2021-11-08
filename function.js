163d119e1294eacb11e04428f9a8229787bbd22c
Aladiah96 <63356947+Aladiah96@users.noreply.github.com>
2021-11-08 11:27:47 -0300

    Btn Problem

--- function.js
+++ function.js
@@ -1,7 +1,17 @@
-function toggle_visibility(resposta) {
-    var e = document.getElementsByClassName("resposta");
-    if(e.style.display == 'block')
-       e.style.display = 'none';
-    else
-       e.style.display = 'block';
- }
+window.addEventListener("load", function () {
+const titulo = document.querySelector(".titulo")
+const resposta = document.querySelector(".resposta")
+const seta = document.querySelector(".seta")
+
+titulo.addEventListener ("click", e => {
+    if (resposta.style.display == "none") {
+            resposta.style.display = "block"
+            titulo.style.color = "black"
+            seta.style.transform= "scaleY(-1)"
+     } else {
+            resposta.style.display = "none"
+            titulo.style.color = "#787887"
+            seta.style.transform= "scaleY(1)"
+     }
+})
+});
